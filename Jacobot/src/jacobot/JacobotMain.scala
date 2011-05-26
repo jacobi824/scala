3ccb499701dd967b3720f891beb59d88e41a5760
Yuki Noma <jacobi824@gmail.com>
2011-05-26 16:57:23 +0900

    Jacobot in Scala

---
+++ Jacobot/src/jacobot/JacobotMain.scala
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jacobot
+
+object JacobotMain {
+
+  /**
+   * @param args the command line arguments
+   */
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+
+}
