117e5886c5f67bc4f3296a4137e114c950c7b703
Yuki Noma <jacobi824@gmail.com>
2011-05-27 23:21:21 +0900

    onMessageTest create Conf.scala

---
+++ Jacobot/src/jacobot/Conf.scala
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jacobot
+
+object Conf {
+  val ServerName = "hogehoge"
+  val ServerEnc = "UTF-8"
+
+  val BotName = "jacobot"
+  val BotVersion = "fuga"
+  val BotFinger = "Finger"
+  val BotLogin = "Login"
+}
--- Jacobot/src/jacobot/JacobotMain.scala
+++ Jacobot/src/jacobot/JacobotMain.scala
@@ -5,14 +5,36 @@
 
 package jacobot
 
-object JacobotMain {
+import java.util.Date
+import org.jibble.pircbot.PircBot
+
+object Jacobot {
 
   def main(args: Array[String]): Unit = {
+
     val bot = new Jacobot
     bot.setVerbose(true)
-    bot.setEncoding("UTF-8")
-    bot.connect("irc.o-in.dwango.co.jp")
-    bot.joinChannel("#komitest")
+
+    bot.setEncoding(Conf.ServerEnc)
+    bot.connect(Conf.ServerName)
+    bot.joinChannel("#jacotest")
   }
 
+}
+
+class Jacobot extends PircBot{
+  this.setName(Conf.BotName)
+  this.setVersion(Conf.BotVersion)
+  this.setFinger(Conf.BotFinger)
+  this.setLogin(Conf.BotLogin)
+
+  override def onMessage(channel: String, sender: String, login: String, hostname: String, message: String) {
+    message match {
+      case "おはよう" => sendNotice(channel, "おはよう" + sender)
+      case "今何時？" => sendNotice(channel, new Date().toString)
+      case "さようなら" => quitServer
+      case "わけあり" => quitServer("サラダバー")
+    }
+
+  }
 }
